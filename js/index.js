84f2ff7ea8505ade66667a546aa7b6a68e0b15be
migee219 <mingee219@gmail.com>
2024-01-08 15:19:16 +0900

    modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#header").load("include/header.html");
-    $("#footer").load("include/bottom.html");
+    $("#header").load("/include/header.html");
+    $("#footer").load("/include/bottom.html");
 });
